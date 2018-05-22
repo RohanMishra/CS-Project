f7385416be92692013cfab8712ffb7a4be2a3c4f
RohanMishra <36312090+RohanMishra@users.noreply.github.com>
2018-05-21 20:03:02 -0400

    Update Constants.java

--- Constants.java
+++ Constants.java
@@ -1,21 +1,22 @@
 /*************************************************************************************
 Interface which every class will implement which has certain constants. Makes it easier to change a constant if something must be changed
 *************************************************************************************/
+import javax.swing.ImageIcon;
 public Interface Constants
 {
-	private int boardWidth = 600;
-	private int boardHeight = 800;
-	private int playerWidth = 100;
-	private int playerHeight = 100;
-	private int enemyWidth = 100;
-	private int enemyHeight = 100;
-	private int powerupWidth = 50;
-	private int powerupHeight = 50;
-	private int laserWidth = 20;
-	private int laserHeight = 100;
-	private ImageIcon playerImage = “player.jpg”;
-	private ImageIcon enemyImage = “enemy.jpg”;
-	private ImageIcon powerupImage = “powerup.jpg”;
-	private ImageIcon laserImage = “laser.jpg”;
-	private ImageIcon backgroundImage = “background.jpg”;
+	public static final int boardWidth = 600;
+	public static final int boardHeight = 800;
+	public static final int playerWidth = 100;
+	public static final int playerHeight = 100;
+	public static final int enemyWidth = 100;
+	public static final int enemyHeight = 100;
+	public static final int powerupWidth = 50;
+	public static final int powerupHeight = 50;
+	public static final int laserWidth = 20;
+	public static final int laserHeight = 100;
+	public static final String playerImage = "player.jpg";
+	public static final String enemyImage = "enemy.jpg";
+	public static final String powerupImage = "powerup.jpg";
+	public static final String laserImage = "laser.jpg";
+	public static final String backgroundImage = "background.jpg";
 }
